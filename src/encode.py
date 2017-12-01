a12f3a62c64caad45e4a5bc82923ae0eedcdf6c1
Muhammet Öztürk <ozturkmuhammeet@gmail.com>
2017-12-01 14:37:17 +0300

    New Branch Created

---
+++ src/encode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import hashlib
+import base64
+
+class Password:
+    def __init__(self,platform,first_key,second_key,third_key,level):
+        self.platform = platform
+        self.first_key = first_key
+        self.second_key = second_key
+        self.third_key = third_key
+        self.level = level
+
+    def display(self):
+
+    def update(self):
+
+    def copy(self):
+
+    def generate(self):
+
---
+++ src/users.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class User:
+    def __init__(self,name,username,email):
+        self.name = name
+        self.username = username
+        self.email = email
+
+    def update(self):
+
+    def remove(self):
+
+    @property
+    def display(self):
+        return
+
